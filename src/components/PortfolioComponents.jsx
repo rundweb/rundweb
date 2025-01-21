f367e0263ca2ffd2618422715e436b335459a54b
faldo <pn4066283@gmail.com>
2025-01-21 11:27:53 +0700

    done all

--- src/components/PortfolioComponents.jsx
+++ src/components/PortfolioComponents.jsx
@@ -1,41 +1,45 @@
 import { useState } from "react";
-import portfolio1 from "../assets/images/fortpolio (1).jpg";
-import portfolio2 from "../assets/images/fortpolio (2).jpg";
-import portfolio3 from "../assets/images/fortpolio (3).jpg";
+import portfolio1 from "../assets/images/rundpay.jpg";
+import portfolio2 from "../assets/images/rundmost.jpg";
+import portfolio3 from "../assets/images/rundshop.jpg";
 import { HiClipboardList } from "react-icons/hi";
 import { AiFillLike } from "react-icons/ai";
 import { FaFire } from "react-icons/fa";
 import { RiMedalFill } from "react-icons/ri";
 import { MdNewLabel } from "react-icons/md";
 import { IoStar } from "react-icons/io5";
+import { FaArrowRight } from "react-icons/fa6";
 const PortfolioComponents = () => {
   const items = [
     {
       id: 1,
       name: "RundPay, By RundWeb",
       sale: 23,
-      stars: 5.0,
+      stars: 4.7,
       price:"$12 - $20",
       image: portfolio1,
       category: ["Bestsellers", "Popular", "Recommendation"],
+      link : "https://rundweb.github.io/Rundpay/"
     },
     {
       id: 2,
       name: "RundMost, By RundWeb",
       sale: 27,
-      stars: 5.0,
+      stars: 4.8,
       price:"$12 - $20",
       image: portfolio2,
       category: ["Popular", "Recommendation"],
+      link : "https://rundweb.github.io/rundmost/"
     },
     {
       id: 3,
       name: "RundShop, By RundWeb",
       sale: 17,
       stars: 5.0,
-      price:"$12 - $20",
+      price:"$20 - $34",
       image: portfolio3,
       category: ["Bestsellers", "Recommendation", "New"],
+      link : "https://rundweb.github.io/rundshop/"
     },
   ];
 
@@ -84,14 +88,17 @@ const PortfolioComponents = () => {
         </button>
       </div>
 
-      <div className="flex flex-wrap gap-10 justify-around mt-5">
+      <div className="flex flex-wrap gap-5 justify-around mt-5">
         {filteredItems.length > 0 ? (
           filteredItems.map((item) => (
-            <div key={item.id} className="neomorphism rounded-xl overflow-hidden">
-              <div className="">
-                <img src={item.image} alt="" className="h-56 w-80 object-cover"/>
+            <div key={item.id} className="neomorphism rounded-xl overflow-hidden relative group cursor-pointer">
+              <div className="flex items-center justify-center absolute w-full h-full top-0 left-0 group-hover:bg-slate-700 group-hover:bg-opacity-50">
+                  <a href={item.link} target="blank" className="hidden group-hover:flex items-center font-medium gap-2 bg-white py-3 px-7 rounded-xl text-primary"><FaArrowRight /> Live Priview</a>
               </div>
-              <div className="p-6 flex flex-col gap-2 text-primary ">
+              <div className="">
+                <img src={item.image} alt="" className="h-56 w-full sm:w-[340px] object-cover object-top"/>
+              </div>
+              <div className="p-6 flex flex-col gap-2 text-primary">
                 <h1 className="font-semibold tracking-wide">{item.name}</h1>
                 <div className="flex items-center text-sm gap-2 my-2">
                   <h2>Over {item.sale} Sales</h2>
